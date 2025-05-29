0094d08402baaee8e150d17dc7ff06d5ad6879f3
omerhalid <omerhalidcinar@gmail.com>
2025-05-29 23:52:47 +0200

    Add initial project files and basic structure
    - Create settings for file associations in VSCode
    - Implement common types and versioning in common.hpp
    - Add main function to output a greeting message
    - Set up a basic sanity test using Catch2

---
+++ include/common.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <cstdint>
+#include <string_view>
+
+using Price = double;
+using Qty   = double;
+using Ts    = std::uint64_t;
+
+constexpr auto version = "TriArbBot 0.0.1";
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "common.hpp"
+#include <iostream>
+
+int main(){
+    std::cout<<"Hello, Binance! running "<<version<<'\n';
+    return 0;
+}
