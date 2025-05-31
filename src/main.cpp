37d17916bb5c802bae4c3fcf110db972a50da573
omerhalid <omerhalidcinar@gmail.com>
2025-05-31 19:44:19 +0200

    Fix compilation error in main.cpp by correcting boost::asio usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
 #include "common.hpp"
+#include "websocket_session.hpp"
+#include <boost/asio.hpp>
 #include <iostream>
 
 int main(){
     std::cout<<"Hello, Binance! running "<<version<<'\n';
+
+    boost::asio:io_context ioc;
+
+    WebsocketSession session(
+        ioc,
+        "testnet.binance.vision",   // host
+        "443",                      // port
+        "/stream?streams=btcusdt@depth5@100ms",
+        [](std::string_view frame){
+            std::cout << frame << '\n';      // later we'll push to queue
+        });
+
+    session.run();
+    ioc.run();
+
     return 0;
 }
+ 
